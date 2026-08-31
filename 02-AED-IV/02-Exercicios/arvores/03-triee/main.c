9ed96734db4f7d263e467f9fbee9b8df0dde6870
Cauã Azevedo Santos <2025105231940025@ifgoiano.local>
2026-08-31 16:47:25 -0300

    exc: triee

--- 02-AED-IV/02-Exercicios/arvores/03-triee/main.c
+++ 02-AED-IV/02-Exercicios/arvores/03-triee/main.c
@@ -20,17 +20,27 @@ void inserir (No **raiz, char *palavra) {
         int index = palavra[0] - 'a';
         if ((*raiz)->letras[index] == NULL) (*raiz)->letras[index] = criarNo();
         if (palavra[1] == '\0') {
-            (*raiz)->palavra = 1;
+            (*raiz)->letras[index]->palavra = 1;
             return;
         }
-        inserir ((*raiz)->letras[index], palavra + 1);   
+        inserir (&((*raiz)->letras[index]), palavra + 1);   
+}
+int vetorNULL (No *vtr[]) {
+    for (int i = 0; i < 26; i++) {
+        if (vtr[i] != NULL) return 0;
+    }
+    return 1;
 }
 
 void remover (No **raiz, char *palavra) {
+    if (palavra == NULL) return;
     int i = palavra[0] - 'a';
-    if (palavra[1] != '\0') {
-        remover((*raiz)->letras[i], palavra + 1);
+    if (palavra[0] == '\0') (*raiz)->palavra = 0;
+    
+    remover(&((*raiz)->letras[i]), palavra + 1);
+    
+    if (vetorNULL((*raiz)->letras) && !(*raiz)->palavra) {
+        free(*raiz);
     }
-    if 
-
+   
 }
